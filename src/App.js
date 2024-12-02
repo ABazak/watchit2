9a74883756cee86a7c24077dd374816ef79668c8
Andrew <vinskala@gmail.com>
2024-12-02 20:33:14 +0200

    Change App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Navigation from "./components/Navigation/Navigation";
 function App() {
   return (
     <div className="App">
-    
-        <Navigation />
-     
+      <Navigation />
     </div>
   );
 }
