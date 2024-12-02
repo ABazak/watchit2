8c667afa4a1ad33dd0bd2981b1728d3d6357efb2
Andrew <vinskala@gmail.com>
2024-12-02 20:18:29 +0200

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    
         <Navigation />
-      </header>
+     
     </div>
   );
 }
