801dec3a7afa56ddde12c33944a66385a5a51c2f
Andrew <vinskala@gmail.com>
2024-12-24 13:47:04 +0200

    change SingleFilm

--- src/pages/SingleFilm.jsx
+++ src/pages/SingleFilm.jsx
@@ -1,40 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid2";
+import axios from "axios";
+import Header from "../components/SingleItemHeader/SingleItemHeader";
 import Tabs from "../components/TabsComponent/TabsComponent";
 import DividerLine from "../components/Actors/Divider";
-import axios from "axios";
-import Grid from "@mui/material/Grid2";
-import Header from "../components/SingleItemHeader/SingleItemHeader";
 import CardActor from "../components/Actors/CardActor";
+import "./ActorInfo.css";
 
 const SingleFilm = () => {
   const { filmId } = useParams();
-  const [filmData, setFilmData] = useState("");
+  const [filmData, setFilmData] = useState(null);
+  const [actors, setActors] = useState([]); 
+
+  // Запрос на данные фильма
   useEffect(() => {
-    const FilmsData = async () => {
+    const fetchFilmData = async () => {
       const response = await axios.get(
         `https://watchit-api.onrender.com/shows/${filmId}`
       );
       setFilmData(response.data);
     };
-    FilmsData();
+
+    const fetchActors = async () => {
+      const response = await axios.get(
+        `https://watchit-api.onrender.com/shows/${filmId}/cast`
+      );
+         const uniqueActors = response.data.filter(
+        (actor, index, self) =>
+          index === self.findIndex((a) => a.person.id === actor.person.id)
+      );
+      setActors(uniqueActors);
+    };
+
+    fetchFilmData();
+    fetchActors();
   }, [filmId]);
 
+  if (!filmData) return <div className="loading">Loading...</div>;
+
   return (
-    <>
-      <Grid container sx={{ marginLeft: "50px", width: "1" }}>
-        <Header
-          name={filmData.name}
-          image={filmData.image?.medium}
-          genres={filmData.genres}
-          rating={filmData.rating?.average}
-        />
-        <Tabs />
-        <DividerLine />
-        <CardActor />
-      </Grid>
-    </>
+    <Grid container sx={{ marginLeft: "50px", width: "1" }}>
+      <Header
+        id={filmData.id}
+        name={filmData.name}
+        time={filmData.premiered}
+        image={filmData.image?.medium}
+        genres={filmData.genres}
+        rating={filmData.rating?.average}
+      />
+      <Tabs />
+      <DividerLine />
+      <CardActor actors={actors} />
+    </Grid>
   );
 };
 
