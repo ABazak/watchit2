43aac335721849cb682caf0658cff9aca8f7bcba
Andrew <vinskala@gmail.com>
2025-02-16 00:11:16 +0200

    change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import { Box, Typography, Link, IconButton } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid2"; 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import XIcon from "@mui/icons-material/X";
 import GoogleIcon from "@mui/icons-material/Google";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
+  const iconApp = "/images/iconstor.svg";
+  const iconGoogle = "/images/icongoogle.png";
+
   return (
     <Box
       sx={{
         backgroundColor: "#232323",
         color: "#fff",
-        padding: "1px",
-        // margin: "100vh",
+        padding: { xs: "20px", md: "30px 70px" },
         mt: "auto",
-        // чтобы футер оставался снизу
       }}
     >
       <Grid
-        container
-        spacing={2}
         sx={{
-          textAlign: "left",
-          margin: "30px 0px 30px 70px",
+          display: "grid",
+          gridTemplateColumns: { xs: "1fr", md: "repeat(3, 1fr)" }, 
+          gap: 2,
+          textAlign: { xs: "center", md: "left" },
           color: "#FFF4E6",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
         }}
       >
         {/* Левая часть с ссылками */}
-        <Grid xs={4}>
+        <Box>
           <Box>
             <Link
               href="#"
@@ -58,21 +56,18 @@ const Footer = () => {
               Watch List
             </Link>
           </Box>
-          <Typography
-            variant="body2"
-            sx={{ marginTop: "40px", maxWidth: "800px" }}
-          >
-            © 2024 WATCHIT. All Rights Resserved. All videos and shows on this
-            platform are trademarks of, and all related images and contens are
-            the property, of Streamit Inc. Duplication and copy of this is
-            strictly prohibited. All rights reserved.
+          <Typography variant="body2" sx={{ marginTop: "20px" }}>
+            © 2025 WATCHIT. All Rights Reserved. All videos and shows on this
+            platform are trademarks of, and all related images and contents are
+            the property of Streamit Inc. Duplication and copy of this is
+            strictly prohibited.
           </Typography>
-        </Grid>
+        </Box>
 
-        {/* Центральная часть с иконками социальных сетей */}
-        <Grid xs={4} textAlign="left">
+        {/* Центральная часть с иконками соцсетей */}
+        <Box>
           <Typography variant="body1">Follow Us :</Typography>
-          <Box sx={{ marginTop: "20px" }}>
+          <Box sx={{ marginTop: "10px" }}>
             <IconButton
               href="#"
               sx={{ color: "#fff", background: "#313131", marginRight: "10px" }}
@@ -89,30 +84,35 @@ const Footer = () => {
               href="#"
               sx={{ color: "#fff", background: "#313131", marginRight: "10px" }}
             >
-              <GoogleIcon sx={{ height: "20px", marginRight: "" }} />
+              <GoogleIcon sx={{ height: "20px" }} />
             </IconButton>
             <IconButton href="#" sx={{ color: "#fff", background: "#313131" }}>
               <GitHubIcon sx={{ height: "20px" }} />
             </IconButton>
           </Box>
-        </Grid>
+        </Box>
 
         {/* Правая часть с приложением */}
-        <Grid xs={4} textAlign="left" sx={{ marginRight: "100px" }}>
+        <Box>
           <Typography variant="body1">Watchit App</Typography>
           <Box sx={{ marginTop: "10px" }}>
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+              src={iconGoogle}
               alt="Google Play"
-              style={{ width: "150px", marginRight: "20px", cursor: "pointer" }}
+              style={{
+                width: "150px",
+                height: "50px",
+                marginRight: "10px",
+                cursor: "pointer",
+              }}
             />
             <img
-              src="https://www.churchoftherock.ca/wp-content/uploads/2024/02/download-on-the-app-store-apple-logo-png-transparent.png"
+              src={iconApp}
               alt="App Store"
-              style={{ width: "150px", cursor: "pointer" }}
+              style={{ width: "150px", height: "50px", cursor: "pointer" }}
             />
           </Box>
-        </Grid>
+        </Box>
       </Grid>
     </Box>
   );
