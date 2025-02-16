67af8509e8d8f09ccbac1fe32aea2ac19eecd814
Andrew <vinskala@gmail.com>
2025-02-16 08:55:26 +0200

    change divider

--- src/components/Actors/Divider.jsx
+++ src/components/Actors/Divider.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 function DividerLine() {
-  return <div style={dividerStyle}></div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "1px",
+        background: "linear-gradient(to right, #141414, #515151, #141414)",
+        margin: { xs: "20px 0", sm: "30px 0", md: "50px 0" }, // Адаптивные отступы
+      }}
+    />
+  );
 }
-
-const dividerStyle = {
-  width: "100%",
-  height: "1px",
-  background: "linear-gradient(to right, #141414, #515151, #141414)",
-  margin: "50px 0",
-};
 
 export default DividerLine;
